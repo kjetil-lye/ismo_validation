d0d15e1b300a27863a99e6bd3484042769a1232d
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2020-05-12 18:14:09 +0200

    Fixed typo

--- run_ensemble_projectile_motion.sh
+++ run_ensemble_projectile_motion.sh
@@ -3,4 +3,4 @@ set -e
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/iterative_surrogate_optimization:$(pwd)
 mkdir -p ensemble_output
 cd ensemble_output
-python -m ismo.bin.run_ensemble --script_name submit_projetile_motion.py --source_folder validation/examples/projectile_motion "$@"
+python -m ismo.bin.run_ensemble --script_name submit_projectile_motion.py --source_folder validation/examples/projectile_motion "$@"
